30d0faf56217d6145b1007f105a2f6cf83f22528
babandrey <bab.andrey@gmail.com>
2021-02-05 20:10:36 +0200

    a

--- Assets/Assets/Scripts/Game Manager/LevelLoader.cs
+++ Assets/Assets/Scripts/Game Manager/LevelLoader.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Runtime.CompilerServices;
 
 public class LevelLoader : MonoBehaviour
 {
