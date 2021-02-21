005f80ab2769e847bd431105dfa7e801dd4a39a9
fkeyzuwu <fkeyzuwu@gmail.com>
2021-02-21 21:23:31 +0200

    idk why its asking me to commit those

--- Assets/Assets/Scripts/Music/AudioManager.cs
+++ Assets/Assets/Scripts/Music/AudioManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class AudioManager : MonoBehaviour
 {
@@ -22,12 +23,13 @@ public class AudioManager : MonoBehaviour
 
     public void Play(string name)
     {
-        Sound sound = sounds[name];
         AudioSource audioToPlay = FindAudioSource(name);
 
         if (audioToPlay == null)
         {
             AudioSource newSource = gameObject.AddComponent<AudioSource>();
+            Sound sound = sounds[name];
+
             newSource.clip = sound.clip;
             newSource.volume = sound.volume;
             newSource.pitch = sound.pitch;
@@ -40,9 +42,65 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    public void Play(string name, bool useRandomPitch)
+    {
+        if (useRandomPitch)
+        {
+            AudioSource audioToPlay = FindAudioSource(name);
+
+            if (audioToPlay == null)
+            {
+                AudioSource newSource = gameObject.AddComponent<AudioSource>();
+                Sound sound = sounds[name];
+
+                newSource.clip = sound.clip;
+                newSource.volume = sound.volume;
+                newSource.pitch = Random.Range(0.9f, 1.1f);
+                newSource.loop = sound.loop;
+                newSource.Play();
+            }
+            else
+            {
+                if(audioToPlay.time > audioToPlay.clip.length / 2 || !audioToPlay.isPlaying)
+                {
+                    audioToPlay.pitch = Random.Range(0.9f, 1.1f);
+                    audioToPlay.Play();
+                }
+            }
+        }
+        else
+        {
+            Play(name);
+        }
+    }
+
+    public void PlayOneShot(string name)
+    {
+        AudioSource audioToPlay = FindAudioSource(name);
+
+        if (audioToPlay == null)
+        {
+            AudioSource newSource = gameObject.AddComponent<AudioSource>();
+            Sound sound = sounds[name];
+
+            newSource.clip = sound.clip;
+            newSource.volume = sound.volume;
+            newSource.loop = sound.loop;
+
+            newSource.pitch = Random.Range(0.9f, 1.1f);
+            newSource.PlayOneShot(newSource.clip);
+        }
+        else
+        {
+            audioToPlay.pitch = Random.Range(0.9f, 1.1f);
+            audioToPlay.PlayOneShot(audioToPlay.clip);
+        }
+    }
+
     public void Stop(string name)
     {
         AudioSource audioToStop = FindAudioSource(name);
+        //audioToStop?.Stop();
         if(audioToStop != null)
         {
             audioToStop.Stop();
